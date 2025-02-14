c61ea63751aaf21ab133031dfafac33188b6dd94
Vinixyi <ismael09870@gmail.com>
2025-02-14 17:54:29 -0400

    Delete src directory

--- src/main/java/com/empresa/recursos_humanos/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // Inicializar listas de datos y simular el proceso de contratación de candidatos
-        System.out.println("Sistema de Recursos Humanos");
-        // Aquí se pueden agregar más funcionalidades y lógica de negocio
-    }
-}
